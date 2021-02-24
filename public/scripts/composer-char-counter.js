1b3c21c888462ab6c4a4f45573380712d27139f9
Ghulam Murtaza <ghulam.murtaza@mail.mcgill.ca>
2021-02-24 00:23:19 +0000

    Tweet form created along with past tweets layout

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  console.log("Doc ready");
+  $('form').on('submit', function(event) {
+    event.preventDefault();
+  })
+
+  //tweetForm.on..
+  $(".tweet-text").on('keyup', function(event) {
+
+    let $input = $(this);
+    let $form = $input.closest('form');
+    let $counter = $form.find(".counter");
+    let len = $input.val().length;
+    let remLength = 140 - len;
+    $counter.html(remLength);
+    if (remLength < 0) {
+      $counter.addClass("red")
+    }
+    else {
+      $counter.removeClass("red");
+    }
+  });
+});
