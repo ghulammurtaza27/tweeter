82e83cf1b83e359ade568d0af688c1e66d946c7b
Ghulam Murtaza <ghulam.murtaza@mail.mcgill.ca>
2021-02-24 16:28:09 +0000

    tweetHTML func added

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,6 +6,46 @@
 
 $(document).ready(function() {
 
+  function tweetHTML(post) {
+    
+    console.log(post);
+    const $tweetText = post.content.text;
+    const oneDay = 24 * 60 * 60 * 1000;
+    const presentDate = new Date();
+    const $UtcTime = post.created_at;
+    const dateCreated = new Date($UtcTime).toLocaleString();
+    const dateOf = new Date(dateCreated);
+    const diffDays = Math.round(Math.abs((dateOf - presentDate) / oneDay));
+    const $userImg = post.user.avatars;
+    const $userName = post.user.name;
+    const $userHandle = post.user.handle;
+    return `
+    <section class="past-tweet">
+      <div class="profile">
+        <div class="profile-info">
+          <img src="${$userImg}" class="tweet-img">
+          <span class="user-name">${$userName}</span>
+        </div>
+        <div class="userID">
+          ${$userHandle}
+        </div>
+      </div>
+      <div class="tweet-txt">
+        ${$tweetText}
+      </div>
+      <div class="tweet-engagement">
+        <p>${diffDays} days ago</p>
+        <div class="tweet-icons">
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
+      </div>
+    </section>
+    `;
+
+  }
+
   function createTweetElement() {
   
     console.log("Doc ready");
@@ -29,41 +69,7 @@ $(document).ready(function() {
     const $main = $('main');
 
     for (post of posts) {
-      console.log(post);
-      const $tweetText = post.content.text;
-      const oneDay = 24 * 60 * 60 * 1000;
-      const presentDate = new Date();
-      const $UtcTime = post.created_at;
-      const dateCreated = new Date($UtcTime).toLocaleString();
-      const dateOf = new Date(dateCreated);
-      const diffDays = Math.round(Math.abs((dateOf - presentDate) / oneDay));
-      const $userImg = post.user.avatars;
-      const $userName = post.user.name;
-      const $userHandle = post.user.handle;
-      const tweetSection = `
-      <section class="past-tweet">
-        <div class="profile">
-          <div class="profile-info">
-            <img src="${$userImg}" class="tweet-img">
-            <span class="user-name">${$userName}</span>
-          </div>
-          <div class="userID">
-            ${$userHandle}
-          </div>
-        </div>
-        <div class="tweet-txt">
-          ${$tweetText}
-        </div>
-        <div class="tweet-engagement">
-          <p>${diffDays} days ago</p>
-          <div class="tweet-icons">
-            <i class="fas fa-flag"></i>
-            <i class="fas fa-retweet"></i>
-            <i class="fas fa-heart"></i>
-          </div>
-        </div>
-      </section>
-      `
+      const tweetSection = tweetHTML(post)
       $main.append(tweetSection);
     }
   };
